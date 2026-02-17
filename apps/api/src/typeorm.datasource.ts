01332882638ad54d7387cec2287ceedd93a3aeb2
Aliaksandr Famchankou <afamchankou@gmail.com>
2026-02-17 19:53:38 +0100

    chore: fix env connect db

--- apps/api/src/typeorm.datasource.ts
+++ apps/api/src/typeorm.datasource.ts
@@ -4,9 +4,14 @@ import { DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Post } from './entities/post.entity';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const databasePath = isProduction
+  ? join(__dirname, 'data', 'microblog.db')
+  : join(process.cwd(), 'data', 'microblog.db');
+
 export const AppDataSource = new DataSource({
   type: 'better-sqlite3',
-  database: join(process.cwd(), 'data', 'microblog.db'),
+  database: databasePath,
   entities: [User, Post],
   migrations: [join(process.cwd(), 'dist', 'migrations', '*.js')],
   synchronize: false,
